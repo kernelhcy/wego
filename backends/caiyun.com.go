f5b10192baa6538ae504b510d82fe358556563f1
hcy <huangcongyu2006@gmail.com>
2020-04-16 00:03:38 +0800

    update indents

--- backends/caiyun.com.go
+++ backends/caiyun.com.go
@@ -1,135 +1,135 @@
 package backends
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"github.com/kernelhcy/wego/iface"
-	"io/ioutil"
-	"log"
-	"net/http"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "github.com/kernelhcy/wego/iface"
+    "io/ioutil"
+    "log"
+    "net/http"
 )
 
 const (
-	caiyunUri="https://api.caiyunapp.com/v2.5/%s/%f,%f/weather.json?alert=true"
+    caiyunUri="https://api.caiyunapp.com/v2.5/%s/%f,%f/weather.json?alert=true"
 )
 
 type cyConfig struct {
-	token string
-	latitude  float64
-	longitude float64
+    token string
+    latitude  float64
+    longitude float64
 }
 
 type cyLifeIndex struct {
-	Index 	float32	`json:"index"`
-	Desc 	string	`json:"desc"`
+    Index 	float32	`json:"index"`
+    Desc 	string	`json:"desc"`
 }
 
 type cyLifeIndices struct {
-	Ultraviolet cyLifeIndex	`json:"ultraviolet"`
-	Comfort 	cyLifeIndex	`json:"comfort"`
+    Ultraviolet cyLifeIndex	`json:"ultraviolet"`
+    Comfort 	cyLifeIndex	`json:"comfort"`
 }
 
 type cyAqi struct {
-	Chn float32	`json:"chn"`
-	Usa float32	`json:"usa"`
+    Chn float32	`json:"chn"`
+    Usa float32	`json:"usa"`
 }
 
 type cyAqiDescription struct {
-	Chn string	`json:"chn"`
-	Usa string	`json:"usa"`
+    Chn string	`json:"chn"`
+    Usa string	`json:"usa"`
 }
 
 type cyRealtimeAirQuality struct {
-	Pm25 float32          `json:"pm25"`
-	Pm10 float32          `json:"pm10"`
-	O3   float32          `json:"o3"`
-	SO2  float32          `json:"so2"`
-	NO2  float32          `json:"no2"`
-	CO   float32          `json:"co"`
-	Aqi  cyAqi            `json:"aqi"`
-	Desc cyAqiDescription `json:"description"`
+    Pm25 float32          `json:"pm25"`
+    Pm10 float32          `json:"pm10"`
+    O3   float32          `json:"o3"`
+    SO2  float32          `json:"so2"`
+    NO2  float32          `json:"no2"`
+    CO   float32          `json:"co"`
+    Aqi  cyAqi            `json:"aqi"`
+    Desc cyAqiDescription `json:"description"`
 }
 
 type cyRealtimeWind struct {
-	Speed 		float32	`json:"speed"`
-	Direction 	float32	`json:"direction"`
+    Speed 		float32	`json:"speed"`
+    Direction 	float32	`json:"direction"`
 }
 
 type cyRealtime struct {
-	Status 				string					`json:"status"`
-	Temperature 		float32					`json:"temperature"`
-	Humidity 			float32					`json:"humidity"`
-	CloudRate 			float32					`json:"cloudrate"`
-	Skycon 				string					`json:"skycon"`
-	Visibility 			float32					`json:"visibility"`
-	Dswrf 				float32					`json:"dswrf"`
-	Wind 				cyRealtimeWind			`json:"wind"`
-	Pressure 			float32					`json:"pressure"`
-	ApparentTemperature float32					`json:"apparent_temperature"`
-	Aqi 				cyRealtimeAirQuality	`json:"air_quality"`
-	LifeIndex 			cyLifeIndices			`json:"life_index"`
+    Status 				string					`json:"status"`
+    Temperature 		float32					`json:"temperature"`
+    Humidity 			float32					`json:"humidity"`
+    CloudRate 			float32					`json:"cloudrate"`
+    Skycon 				string					`json:"skycon"`
+    Visibility 			float32					`json:"visibility"`
+    Dswrf 				float32					`json:"dswrf"`
+    Wind 				cyRealtimeWind			`json:"wind"`
+    Pressure 			float32					`json:"pressure"`
+    ApparentTemperature float32					`json:"apparent_temperature"`
+    Aqi 				cyRealtimeAirQuality	`json:"air_quality"`
+    LifeIndex 			cyLifeIndices			`json:"life_index"`
 }
 
 type cyWeatherResult struct {
-	Realtime cyRealtime	`json:"realtime"`
+    Realtime cyRealtime	`json:"realtime"`
 }
 
 type cyWeatherData struct {
-	Status 		string				`json:"status"`
-	ApiVersion 	string				`json:"api_version"`
-	ApiStatus 	string				`json:"api_status"`
-	Lang 		string				`json:"lang"`
-	Unit 		string				`json:"unit"`
-	Tzshift 	int32				`json:"tzshift"`
-	Timezone 	string				`json:"timezone"`
-	ServerTime 	int32				`json:"server_time"`
-	Location 	[2]float32			`json:"location"`
-	Result 		cyWeatherResult		`json:"result"`
+    Status 		string				`json:"status"`
+    ApiVersion 	string				`json:"api_version"`
+    ApiStatus 	string				`json:"api_status"`
+    Lang 		string				`json:"lang"`
+    Unit 		string				`json:"unit"`
+    Tzshift 	int32				`json:"tzshift"`
+    Timezone 	string				`json:"timezone"`
+    ServerTime 	int32				`json:"server_time"`
+    Location 	[2]float32			`json:"location"`
+    Result 		cyWeatherResult		`json:"result"`
 }
 
 func (c *cyConfig) fetch() (*cyWeatherData, error) {
-	url := fmt.Sprintf(caiyunUri, c.token, c.longitude, c.latitude)
-	log.Printf("caiyun url: %s\n", url)
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("unable to get (%s): %v", url, err)
-	} else if res.StatusCode != 200 {
-		return nil, fmt.Errorf("unable to get (%s): http status %d", url, res.StatusCode)
-	}
-	defer res.Body.Close()
+    url := fmt.Sprintf(caiyunUri, c.token, c.longitude, c.latitude)
+    log.Printf("caiyun url: %s\n", url)
+    res, err := http.Get(url)
+    if err != nil {
+        return nil, fmt.Errorf("unable to get (%s): %v", url, err)
+    } else if res.StatusCode != 200 {
+        return nil, fmt.Errorf("unable to get (%s): http status %d", url, res.StatusCode)
+    }
+    defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("unable to read response body (%s): %v", url, err)
-	}
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        return nil, fmt.Errorf("unable to read response body (%s): %v", url, err)
+    }
 
-	var data cyWeatherData
-	if err = json.Unmarshal(body, &data); err != nil {
-		return nil, fmt.Errorf("unable to unmarshal response (%s): %v\nThe json body is: %s", url, err, string(body))
-	}
+    var data cyWeatherData
+    if err = json.Unmarshal(body, &data); err != nil {
+        return nil, fmt.Errorf("unable to unmarshal response (%s): %v\nThe json body is: %s", url, err, string(body))
+    }
 
-	return &data, nil
+    return &data, nil
 }
 
 func (c *cyConfig) Setup() {
-	flag.StringVar(&c.token, "caiyun-api-token", "", "caiyun backend: the api `TOKEN` to use")
-	flag.Float64Var(&c.latitude, "caiyun-latitude", 30.274085, "caiyun backend: the `latitude` to request from caiyun.com")
-	flag.Float64Var(&c.longitude, "caiyun-longitude", 120.15507, "caiyun backend: the `longitude` to request from caiyun.com")
+    flag.StringVar(&c.token, "caiyun-api-token", "", "caiyun backend: the api `TOKEN` to use")
+    flag.Float64Var(&c.latitude, "caiyun-latitude", 30.274085, "caiyun backend: the `latitude` to request from caiyun.com")
+    flag.Float64Var(&c.longitude, "caiyun-longitude", 120.15507, "caiyun backend: the `longitude` to request from caiyun.com")
 }
 
 func (c *cyConfig) Fetch(location string, numdays int) iface.Data {
-	data, err := c.fetch()
-	if err != nil {
-		log.Printf("caiyun fetch error: %v\n", err)
-		return iface.Data{}
-	}
+    data, err := c.fetch()
+    if err != nil {
+        log.Printf("caiyun fetch error: %v\n", err)
+        return iface.Data{}
+    }
 
-	fmt.Printf("%+v\n", data)
+    fmt.Printf("%+v\n", data)
 
-	return iface.Data{}
+    return iface.Data{}
 }
 
 func init() {
-	iface.AllBackends["caiyun.com"] = &cyConfig{}
+    iface.AllBackends["caiyun.com"] = &cyConfig{}
 }
